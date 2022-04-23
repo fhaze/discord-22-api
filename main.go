690117ca4b7a808932716c5d43a57f626a5a4313
fhaze <eder_arm@icloud.com>
2022-04-23 16:19:27 -0300

    adding cors

--- main.go
+++ main.go
@@ -4,16 +4,21 @@ import (
 	"discord-22-api/config"
 	"discord-22-api/ctx"
 	"discord-22-api/router"
+	"net/http"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/sirupsen/logrus"
-	"net/http"
 )
 
 func main() {
 	logrus.SetFormatter(&logrus.JSONFormatter{DisableTimestamp: true})
 
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+	}))
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{Format: `{"id":"${id}","method":"${method}","uri":"${uri}","user_agent":"${user_agent}",` +
 		`"status":${status},"latency":"${latency}","bytes_in":${bytes_in},"bytes_out":${bytes_out}}` + "\n"}))
 	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
